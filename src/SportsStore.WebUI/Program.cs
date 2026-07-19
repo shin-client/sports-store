4635f4946686ab387b8e8807b16b34f6b0925a26
ngxc <ngoc.shintrann@gmail.com>
2026-07-20 05:40:55 +0700

    Initial project

--- src/SportsStore.WebUI/Program.cs
+++ src/SportsStore.WebUI/Program.cs
@@ -1,10 +1,12 @@
 using SportsStore.Domain.Interfaces;
 using SportsStore.Application.Concrete;
+using SportsStore.WebUI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddScoped<IProductRepository, MockProductRepository>();
 
 var app = builder.Build();
@@ -12,23 +14,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
-app.UseRouting();
 
-app.UseAuthorization();
+app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.UseStaticFiles();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
