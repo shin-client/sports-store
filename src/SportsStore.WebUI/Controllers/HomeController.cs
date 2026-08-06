55b7c7b649e59d7c971af5d70901ff9161c30b38
ngxc <ngoc.shintrann@gmail.com>
2026-08-06 09:58:05 +0700

    feat(web): wire up category filtering, cart session DI, and store layout UI

--- src/SportsStore.WebUI/Controllers/HomeController.cs
+++ src/SportsStore.WebUI/Controllers/HomeController.cs
@@ -8,16 +8,32 @@ namespace SportsStore.WebUI.Controllers;
 public class HomeController : Controller
 {
     private readonly IProductRepository _repository;
+    public int PageSize = 4;
 
     public HomeController(IProductRepository repository)
     {
         _repository = repository;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string? category, int productPage = 1)
     {
-        var products = _repository.Products.ToList();
-        return View(products);
+        return View(new ProductsListViewModel
+        {
+            Products = _repository.Products
+                .Where(p => category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = category == null
+                    ? _repository.Products.Count()
+                    : _repository.Products.Where(e => e.Category == category).Count()
+            },
+            CurrentCategory = category
+        });
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- src/SportsStore.WebUI/Program.cs
+++ src/SportsStore.WebUI/Program.cs
@@ -1,5 +1,7 @@
 using SportsStore.Application.Concrete;
+using SportsStore.Domain;
 using SportsStore.Domain.Interfaces;
+using SportsStore.WebUI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,22 +9,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IProductRepository, MockProductRepository>();
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
